c0d6a430a0f20e66496d249cad491d11e71f5b6c
MarcusCollin <gkia.yt@gmail.com>
2021-03-18 14:31:29 +0100

    afsasf

--- lua/babi/tasks/RelationalConjunction.lua
+++ lua/babi/tasks/RelationalConjunction.lua
@@ -63,7 +63,7 @@ function RelationalConjunction:generate_story(world, knowledge, story)
         story:append(babi.Question(
             'yes_no',
             babi.Clause(world, truth_value, world:god(), actions.set,
-                   location2, '=', location1),
+                   random_actor2, 'is_in', location1),
             support1
         ))
         -- local clause = babi.Clause(
