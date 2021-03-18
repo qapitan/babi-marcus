6c17395294b6be597e919a44c3af49790146b0e4
MarcusCollin <gkia.yt@gmail.com>
2021-03-18 19:40:07 +0100

    asfasf

--- lua/babi/tasks/RelationalConjunction.lua
+++ lua/babi/tasks/RelationalConjunction.lua
@@ -70,6 +70,6 @@ function RelationalConjunction:generate_story(world, knowledge, story)
     return story, knowledge
 end
 
-RelationalConjunction.DEFAULT_CONFIG = {conjunction = 0.9}
+RelationalConjunction.DEFAULT_CONFIG = {conjunction = 0.5}
 
 return RelationalConjunction
