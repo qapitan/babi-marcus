14bbe6146a6f6cf802b2fcc556ffc6a8bcf7af9a
MarcusCollin <gkia.yt@gmail.com>
2021-03-30 16:35:01 +0200

    asfa

--- lua/babi/tasks/RelationalConjunction.lua
+++ lua/babi/tasks/RelationalConjunction.lua
@@ -67,7 +67,7 @@ function RelationalConjunction:generate_story(world, knowledge, story)
             babi.Question(
                 "yes_no",
                 babi.Clause(world, truth_value, world:god(), actions.set, random_actor2, "is_in", random_actor1),
-                support2
+                Set(support1,support2)
             )
         )
     end
