d327721159a26a28786ba32897acdbabf98327d1
MarcusCollin <gkia.yt@gmail.com>
2021-03-30 19:53:55 +0200

    fasfas

--- lua/babi/tasks/RelationalConjunction.lua
+++ lua/babi/tasks/RelationalConjunction.lua
@@ -67,6 +67,15 @@ function RelationalConjunction:generate_story(world, knowledge, story)
         -- knowledge:current()[random_actor]:get_value('is_in', true)
         local truth_value = (location1 == location2) -- Are the two actors same place?
         
+        if (support1 == support2){
+                story:append(
+                babi.Question(
+                    "yes_no",
+                    babi.Clause(world, truth_value, world:god(), actions.set, random_actor2, "is_in", random_actor1),
+                    support1
+                )
+            )
+        }
         story:append(
             babi.Question(
                 "yes_no",
