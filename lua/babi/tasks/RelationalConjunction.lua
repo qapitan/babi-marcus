e25e79928e0c49d9170a6928733c6183612b5573
MarcusCollin <gkia.yt@gmail.com>
2021-03-30 18:05:44 +0200

    asfasfa

--- lua/babi/tasks/RelationalConjunction.lua
+++ lua/babi/tasks/RelationalConjunction.lua
@@ -70,8 +70,8 @@ function RelationalConjunction:generate_story(world, knowledge, story)
                 support1
             )
         )
-        print(support1[0])
-        print(type(support1[0]))
+        print(support1[1])
+        print(type(support1[1]))
     end
     return story, knowledge
 end
